d2c3caec808d8e3e17da245710d65c4d201205c6
prateek <113834812+testlearnprat@users.noreply.github.com>
2022-11-02 12:45:48 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ void main()
                 printf("\nCalling hello function");
                 hello();
                 printf("\nCalling add function");
-                add(10,4);
+                add(1,4);
                 printf("\nBack in main.c\n");
 }
